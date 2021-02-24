efc9f54923b8f5e05052260ed14884d1fa380fa6
AymenBida <aymen.bida.00@gmail.com>
2021-02-24 16:28:15 +0100

    Add model methods for users to work with friendships

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,28 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+
+  def friends
+    friends_array = friendships.map { |f| f.friend if f.status == 'confirmed' }
+    inverse_friends = inverse_friendships.map { |f| f.user if f.status == 'confirmed' }
+    all_friends = friends_array + inverse_friends
+  end
+
+  def pending_friends
+    friendships.map { |f| f.friend if f.status == 'pending' }
+  end
+
+  def friend_requests
+    inverse_friendships.map { |f| f.user if f.status == 'pending' }
+  end
+
+  def confirm_friend(user)
+    friendship = inverse_friendships.find { |f| f.user == user }
+    friendship.status = 'confirmed'
+    friendship.save
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
 end
