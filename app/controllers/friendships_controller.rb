832f575b7309ac41f131f14cb6bbd4781f5742fb
AymenBida <aymen.bida.00@gmail.com>
2021-02-25 11:12:50 +0100

    Add invite to friendship button

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,13 @@
 class FriendshipsController < ApplicationController
+  def create
+    @user = User.find(params[:user_id])
+    @friendship = current_user.friendships.build(friend_id: @user.id, status: 'pending')
+    if @friendship.save
+      flash[:notice] = "Your friend request to #{@user.name} is sent!"
+      redirect_to users_path
+    else
+      flash[:alert] = "We didn't succeed to send your request!"
+      render '/users'
+    end
+  end
 end
