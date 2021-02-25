80f53302e025109d3dc916f0174ff8194912fc78
AymenBida <aymen.bida.00@gmail.com>
2021-02-25 14:02:30 +0100

    Friendships: remove the possiblity to invite the same user multiple times

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,13 +1,19 @@
 class FriendshipsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
+    @already_invited = current_user.pending_friends.select { |user| user.id == @user.id }
     @friendship = current_user.friendships.build(friend_id: @user.id, status: 'pending')
-    if @friendship.save
-      flash[:notice] = "Your friend request to #{@user.name} is sent!"
+    if !@already_invited.empty?
+      flash[:alert] = "You already sent a request to #{@user.name}!"
       redirect_to users_path
     else
-      flash[:alert] = "We didn't succeed to send your request!"
-      render '/users'
+      if @friendship.save
+        flash[:notice] = "Your friend request to #{@user.name} is sent!"
+        redirect_to users_path
+      else
+        flash[:alert] = "We didn't succeed to send your request!"
+        redirect_to users_path
+      end
     end
   end
 
