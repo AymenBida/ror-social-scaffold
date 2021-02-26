ba196010933f0b07f474343c3355156758ac19fc
AshakaE <ashakaegerega@gmail.com>
2021-02-26 10:27:03 +0100

    create model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "should not save user without params" do
+  test 'should not save user without params' do
     user = User.new
     assert_not user.save
   end
