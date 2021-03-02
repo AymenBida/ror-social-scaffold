4e8b6b23925b4c7f10fea3b91dda83e3db0ca945
AshakaE <ashakaegerega@gmail.com>
2021-03-02 10:12:11 +0100

    create accept friendship test

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -14,16 +14,23 @@ RSpec.describe Friendship, type: :model do
 end
 
 RSpec.feature 'Friendships' do
-  let(:current_user) { User.create(name: 'Yukihiro Matsumo', email: 'yukihiro@mastsumoto.com', password: 'matsumoto') }
+  let(:current_user) { User.create(id: 1, name: 'Yukihiro Matsumo', email: 'yukihiro@mastsumoto.com', password: 'matsumoto') }
 
-  let(:another_user) { User.create(name: 'Yubei Miyazaki', email: 'yubei@miyazaki.com', password: 'miyazaki') }
+  let(:another_user) { User.create(id: 2, name: 'Yubei Miyazaki', email: 'yubei@miyazaki.com', password: 'miyazaki') }
+
+  let(:third_user) { User.create(id: 3, name: 'luigi Mahrez', email: 'luigi@mahrez.com', password: 'mahrez') }
+  let(:fourth_user) { User.create(id: 4, name: 'Jorge Martin', email: 'jorge@martin.com', password: 'martin') }
+
+  let(:invitation) { Friendship.create(user_id: 3, friend_id: 4, status: 'pending')}
 
   before(:each) do
     current_user
-  end
-  before(:each) do
     another_user
+    third_user
+    fourth_user
+    invitation
   end
+
 
   scenario 'when a user gets invited to a friendship' do
     visit '/users/sign_in'
@@ -31,7 +38,20 @@ RSpec.feature 'Friendships' do
     fill_in 'Password', with: 'matsumoto'
     click_on 'Log in'
     visit '/users'
-    click_on 'Invite to friendship'
+    click_link 'users/2/friendships'
+    # click_on 'Invite to friendship'
     expect(page).to have_content("Your friend request to #{another_user.name} is sent!")
+  end
+
+  scenario 'when a user accepts an invitation to a friendship' do
+
+    visit '/users/sign_in'
+    fill_in 'Email', with: 'jorge@martin.com'
+    fill_in 'Password', with: 'martin'
+    click_on 'Log in'
+    visit '/users/4/friendships'
+    expect(page).to have_content('Pending requests')
+    # click_on 'Accept friendship'
+    # expect(page).to have_content("You're now friends with to #{current_user.name}!")
   end
 end
