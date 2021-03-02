83987493c844765d0c99ff6e9f7bcca591d3b1fc
AymenBida <aymen.bida.00@gmail.com>
2021-03-02 10:23:03 +0100

    Friendship_spec: fix the accept invitation test

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -15,20 +15,11 @@ end
 
 RSpec.feature 'Friendships' do
   let(:current_user) { User.create(id: 1, name: 'Yukihiro Matsumo', email: 'yukihiro@mastsumoto.com', password: 'matsumoto') }
-
   let(:another_user) { User.create(id: 2, name: 'Yubei Miyazaki', email: 'yubei@miyazaki.com', password: 'miyazaki') }
-
-  let(:third_user) { User.create(id: 3, name: 'luigi Mahrez', email: 'luigi@mahrez.com', password: 'mahrez') }
-  let(:fourth_user) { User.create(id: 4, name: 'Jorge Martin', email: 'jorge@martin.com', password: 'martin') }
-
-  let(:invitation) { Friendship.create(user_id: 3, friend_id: 4, status: 'pending')}
 
   before(:each) do
     current_user
     another_user
-    third_user
-    fourth_user
-    invitation
   end
 
 
@@ -38,20 +29,18 @@ RSpec.feature 'Friendships' do
     fill_in 'Password', with: 'matsumoto'
     click_on 'Log in'
     visit '/users'
-    click_link 'users/2/friendships'
-    # click_on 'Invite to friendship'
+    click_on 'Invite to friendship'
     expect(page).to have_content("Your friend request to #{another_user.name} is sent!")
   end
 
   scenario 'when a user accepts an invitation to a friendship' do
-
+    Friendship.create(user_id: 2, friend_id: 1, status: 'pending')
     visit '/users/sign_in'
-    fill_in 'Email', with: 'jorge@martin.com'
-    fill_in 'Password', with: 'martin'
+    fill_in 'Email', with: 'yukihiro@mastsumoto.com'
+    fill_in 'Password', with: 'matsumoto'
     click_on 'Log in'
-    visit '/users/4/friendships'
-    expect(page).to have_content('Pending requests')
-    # click_on 'Accept friendship'
-    # expect(page).to have_content("You're now friends with to #{current_user.name}!")
+    visit '/users/1/friendships'
+    click_on 'Accept friendship'
+    expect(page).to have_content("You are now friends with #{another_user.name}!")
   end
 end
